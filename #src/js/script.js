ab45ebf00e3565a625c90ad49eabc3ac0bf2a44e
Alexey <evrobud.snab@gmail.com>
2022-06-13 19:47:02 +0300

    create modal window (open-close)

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+	var modalButtons = document.querySelectorAll('.header__btn'),
+		overlay = document.querySelector('.js-overlay-modal'),
+		closeButtons = document.querySelectorAll('.rightside-menu__close');
+
+	modalButtons.forEach(function (item) {
+
+		item.addEventListener('click', function (e) {
+
+			e.preventDefault();
+
+			var modalId = this.getAttribute('data-modal'),
+				modalElem = document.querySelector('.rightside-menu[data-modal="' + modalId + '"]');
+
+			modalElem.classList.add('active');
+			overlay.classList.add('active');
+		});
+	});
+
+
+	closeButtons.forEach(function (item) {
+
+		item.addEventListener('click', function (e) {
+			var parentModal = this.closest('.rightside-menu');
+
+			parentModal.classList.remove('active');
+			overlay.classList.remove('active');
+		});
+	});
+
+	document.body.addEventListener('keyup', function (e) {
+		var key = e.keyCode;
+
+		if (key == 27) {
+
+			document.querySelector('.rightside-menu.active').classList.remove('active');
+			document.querySelector('.overlay').classList.remove('active');
+		};
+	}, false);
+
+	overlay.addEventListener('click', function () {
+		document.querySelector('.rightside-menu.active').classList.remove('active');
+		this.classList.remove('active');
+	});
+});
+
+
+
+
 function testWebP(callback) {
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
